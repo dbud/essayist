9315b88aae7aa54e61ad1e13f773880dd47de409
Dima Budaragin <d.budaragin+dev@gmail.com>
2026-08-06 19:58:33 +0100

    feat(signals/): add navigationOpened signal

--- packages/web/signals/sidebar.ts
+++ packages/web/signals/sidebar.ts
@@ -2,3 +2,4 @@ import { persistentSignal } from "@/utils/persistentSignal.ts";
 
 export const leftSidebarOpened = persistentSignal("leftSidebarOpened", true);
 export const rightSidebarOpened = persistentSignal("rightSidebarOpened", false);
+export const navigationOpened = persistentSignal("navigationOpened", false);
