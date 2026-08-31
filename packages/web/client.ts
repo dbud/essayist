ae02c484986dfa3397ecea5c55b9215570f4bc9e
Dima Budaragin <d.budaragin+dev@gmail.com>
2026-08-31 09:41:13 +0100

    feat(web/): add Atkinson Hyperlegible Next font

--- packages/web/client.ts
+++ packages/web/client.ts
@@ -1,4 +1,6 @@
 import "@fontsource-variable/source-serif-4/standard";
 import "@fontsource-variable/source-serif-4/standard-italic";
+import "@fontsource-variable/atkinson-hyperlegible-next/wght.css";
+import "@fontsource-variable/atkinson-hyperlegible-next/wght-italic.css";
 import "@fontsource-variable/special-gothic/wdth.css";
 import "./assets/styles.css";
