d45b8c70613c412d104414eb7a0d101592e1e103
Dima Budaragin <d.budaragin+dev@gmail.com>
2026-08-31 10:24:49 +0100

    feat(web): add Recursive mono font
    Remove the unused hanken-grotesk variable font dependency

--- packages/web/client.ts
+++ packages/web/client.ts
@@ -2,5 +2,6 @@ import "@fontsource-variable/source-serif-4/standard";
 import "@fontsource-variable/source-serif-4/standard-italic";
 import "@fontsource-variable/atkinson-hyperlegible-next/wght.css";
 import "@fontsource-variable/atkinson-hyperlegible-next/wght-italic.css";
+import "@fontsource-variable/recursive/mono.css";
 import "@fontsource-variable/special-gothic/wdth.css";
 import "./assets/styles.css";
